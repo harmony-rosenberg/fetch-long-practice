77a271521ef2de9f190c980a4ed2a70ad12a3b78
Adrian <adrian.sanchezjr18@github.com>
2023-11-01 13:20:06 -0700

    phase 1 complete

--- fetch-requests.js
+++ fetch-requests.js
@@ -2,10 +2,22 @@
 Make fetch requests in the browser for each of the following phases.
 Paste your code for fetch requests here once you finish each phase.
 */
+// server.js
+const http = require('http');
 
+const server = http.createServer((req, res) => {
+
+});
 /* ============================== Phase 1 ============================== */
 
 // Your code here
+fetch('/products', {
+    method: 'POST',
+    body: "name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    }
+  });
 
 
 
